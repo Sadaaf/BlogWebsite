e6767ebffdcd5e848e77acfb1a43f46e03636ccf
Sadaf Al Arabi <sadafalarabii229@gmail.com>
2022-09-06 02:28:22 +0600

    Change procfile to run app

--- app.js
+++ app.js
@@ -108,6 +108,6 @@ app.get("/posts/:postID", function (req,res) {
   })
 })
 
-app.listen(3000 || process.env.PORT, function () {
+app.listen( process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
